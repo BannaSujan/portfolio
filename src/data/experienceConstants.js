7504a4386ca4fd499654e6fade98cdef2a6afd0f
BannaSujan <bannasaisujan@gmail.com>
2024-04-29 11:58:58 -0400

    experience changes

--- src/data/experienceConstants.js
+++ src/data/experienceConstants.js
@@ -5,31 +5,31 @@ export const experiences = [
         company: "Amazon.com LLC.",
         startDate: "May 2023",
         endDate: "August 2023",
-        description: "At Amazon.com, I spearheaded the development of multi-language hint services for global expansion, streamlining user interactions and significantly expanding market reach. Leveraged Java, Spring Boot, and AWS tools to architect and implement end-to-end solutions, contributing to enhanced user experiences and operational efficiency."
+        description: "I was part of the Alexa Video team at Amazon, where I focused on developing new features to expand the hints services internationally. My key contribution was creating an end-to-end working proof of concept (POC) that enabled multi-language hints, triggered by the payload of a RESTful API. I effectively transformed the project’s initial aim into a viable and executable plan by refactoring the hints architecture, which allowed multi-language hints to be displayed on universal search and home screens. Additionally, I designed the database schema in DynamoDB and implemented the business logic using Java design patterns. This POC significantly enhanced Alexa's hint services, extending them to over 50% of Amazon’s international market."
     },
     {
         id: 2,
         title: "Front-end Developer",
         company: "ValueLabs Pvt. Ltd.",
-        startDate: "June 2020",
+        startDate: "June 2019",
         endDate: "December 2021",
-        description: "As a Frontend Developer at ValueLabs, I spearheaded the creation of intuitive user interfaces for a prominent American manufacturer's mobile application, contributing to a seamless user experience and facilitating post-release engagement with over 30,000 downloads.Assisted in the development of web applications and websites."
+        description: "I developed the backend using Spring Boot, which included integrating various layers such as DAO/repository, Controller, Service, and Persistence. I managed database creation and Hibernate ORM connectivity, implemented Spring authentication, resolved CORS issues, and created unit tests with JUnit while using Postman for RESTful API service testing. On the frontend, I built a React application using stateful class components, Redux for state management, and React lifecycle methods to enhance user experience. I crafted reusable React components, leveraged props for component interaction, and ensured application security with React-Router and Axios for HTTP requests. Additionally, I maintained Git repositories, used JIRA for project tracking, and actively participated in the SDLC and Scrum meetings."
     },
     {
         id: 3,
         title: "SDE Intern",
         company: "ValueLabs Pvt. Ltd.",
-        startDate: "December 2019",
-        endDate: "April 2020",
-        description: "During my internship at ValueLabs, I played a pivotal role in front-end development projects, leveraging ReactJS and SwiftUI to enhance user interfaces and drive post-release engagement "
+        startDate: "December 2018",
+        endDate: "April 2019",
+        description: "I engineered robust web applications using Java/J2EE, focusing on the MVC architecture to improve scalability and maintainability. I excelled in designing SQL schemas and implementing CRUD operations, including formulating advanced stored procedures. Additionally, I applied advanced Java concepts such as Collections, Generics, and Multi-threading, along with error handling, to optimize data management and support concurrent user operations."
     },
     {
         id: 4,
         title: "Junior Software Developer",
         company: "Startup Hub",
-        startDate: "August 2018",
-        endDate: "April 2019",
-        description: "Participated in the development of mobile and web applications in a fast-paced startup environment. Introduced to Agile methodologies and pair programming."
+        startDate: "August 2017",
+        endDate: "April 2018",
+        description: "During this internship, I played a pivotal role in front-end development projects, leveraging ReactJS and other front-end tools to enhance user interfaces and drive post-release engagement "
     }
 ];
 
